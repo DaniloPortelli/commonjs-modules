e46176a5cf6ad7fee49365a19dfc5aeedd0776a9
DaniloPortelli <daniloportelli@gmail.com>
2025-02-06 14:55:43 +0100

    first commit

---
+++ names.js
@@ -0,0 +1,18 @@
+// 1 - names.js
+// 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione 
+// dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// 2. Esporta la funzione dal file.
+
+function fullName(firstName, lastName) {
+    return {
+        firstName: firstName,
+        lastName: lastName
+    }
+}
+
+// console.log(fullName("Danilo", "Portelli"))
+
+module.exports = fullName;
+
+
+
---
+++ people.js
@@ -0,0 +1,21 @@
+// 3 - people.js
+// 1. Importa la tua funzione da names.js
+// 2. Importa la tua funzione da hobbies.js
+// 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. 
+// All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+
+const fullName = require("./names");
+const hobbies = require("./hobbies");
+
+
+// console.log(fullName("Danilo", "Portelli"));
+// console.log(hobbies("Piano", "Trekking", "Reading"));
+
+function aboutMe() {
+    return {
+        fullName: fullName("Danilo", "Portelli"),
+        hobbies: hobbies("Piano", "Trekking", "Reading")
+    }
+}
+
+console.log(aboutMe())
